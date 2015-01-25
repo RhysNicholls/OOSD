e31f1cd601ce7854f3281ec640e69525a632be01
shiftyllama <shiftyllama@googlemail.com>
2015-01-25 17:54:58 +0000

    25/01/2015

---
+++ src/book/chapter1/Question1point5.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package book.chapter1;
+
+/**
+ *
+ * @author Shiftyllama
+ */
+public class Question1point5 {
+    public static void main(String[] args) {
+    System.out.println((7.5 * 2.5 - 1.5 *3)/(35.52.5));
+}
+}
