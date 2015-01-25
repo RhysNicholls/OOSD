03029df8c768eb2486167b980e445dacbe1d6a4a
shiftyllama <shiftyllama@googlemail.com>
2015-01-25 18:00:51 +0000

    25/01/2015

--- src/book/chapter1/Question1point5.java
+++ src/book/chapter1/Question1point5.java
@@ -10,7 +10,7 @@ package book.chapter1;
  * @author Shiftyllama
  */
 public class Question1point5 {
-    public static void main(String[] args) {
-    System.out.println((7.5 * 2.5 - 1.5 *3)/(35.52.5));
-}
+ public static void main(String[] args) {
+       System.out.println((7.5 * 2.5 - 1.5 * 3)/(35.5 - 2.5));
+    }
 }
