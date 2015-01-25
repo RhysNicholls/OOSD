6875363ef8fcbc0578aef8838fa6802f22a33c20
shiftyllama <shiftyllama@googlemail.com>
2015-01-25 17:35:52 +0000

    25/01/2015

---
+++ src/book/chapter1/Question1point4.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package book.chapter1;
+
+/**
+ *
+ * @author Shiftyllama
+ */
+public class Question1point4 {
+  public static void main(String[] args)
+  {
+    System.out.println();
+    System.out.println("a   a*2    a*3");
+    System.out.println("1   2      3");
+    System.out.println("2   4      6");
+    System.out.println("3   6      9");
+    System.out.println("4   8      12");
+    System.out.println();
+}
+}
